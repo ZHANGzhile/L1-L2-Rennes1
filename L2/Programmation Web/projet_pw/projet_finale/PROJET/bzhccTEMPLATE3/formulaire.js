880d4c095dc9a0d8db83cce20d6a33432b00ed92
ji-jiao <79673283+ZHANGzhile@users.noreply.github.com>
2023-04-11 04:39:50 +0200

    L2

--- L2/Programmation Web/projet_pw/projet_finale/PROJET/bzhccTEMPLATE3/formulaire.js
+++
@@ -1,6 +0,0 @@
-function formsubmit() {
-    var textS = document.getElementById('textSearch').value;
-    var sel = document.getElementById('select-1');
-    var seltext = sel.options[sel.selectedIndex].text;
-    return 'search.php?textSearch=' + textS + '&selection=' + seltext;
-}
